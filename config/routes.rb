0f44b76b251f3c26b13867bcbf4b5e39a740e8e1
byzg <byzg00@gmail.com>
2016-08-05 14:32:24 +0300

    Video

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  get :video, controller: :pages
+
   root 'magazines#index'
   resources :magazines, only: [:index, :show]
 end
